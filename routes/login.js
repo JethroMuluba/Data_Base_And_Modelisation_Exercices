e2cd05439948b5db767874fa5204ce3b354a0c3e
JethroMuluba <jethro.muluba@gmail.com>
2024-04-02 20:58:00 +0100

    Feat: Create Login Route

---
+++ routes/login.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const userLogin = require('../middleware/login')
+
+const loginRouter = express.Router();
+
+
+loginRouter.post('/', userLogin)
+
+
+module.exports = loginRouter;  
