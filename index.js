af38941813114104c78e827fa8838cdec1893379
JethroMuluba <jethro.muluba@gmail.com>
2024-04-02 21:04:49 +0100

    Feat: import and use the PORT of dotenv in index.js fil

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const  app = express();
+const dotenv = require('dotenv').config();
 
+const PORT = process.env.PORT;
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port : ${PORT}`)
+})
