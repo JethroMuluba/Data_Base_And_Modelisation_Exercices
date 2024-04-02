591f4967831682184b2c901d0a3f1f460698f417
JethroMuluba <jethro.muluba@gmail.com>
2024-04-02 21:31:40 +0100

    Feat : Install Passport and Passport-local

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const loginRouter = require('./routes/login');
 const  app = express();
 const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT;
 
 
-
+app.use('/login', loginRouter)
 
 
 
